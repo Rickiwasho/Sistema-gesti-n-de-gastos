e5569984b7b99dcca5b6a029f688ebc9e7b279db
Rickiwasho <ricardo.coronado@alumnos.uach.cl>
2020-11-19 00:54:38 -0300

    f***ng navbar

--- src/frontend/ssgastos/src/components/Navbar.js
+++ src/frontend/ssgastos/src/components/Navbar.js
@@ -1,17 +1,72 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
+
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        } else{
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
+
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        SOLUCIONES INTEGRALES <i className="fab fa-typo3"/>
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                        SOLUCIONES INTEGRALES 
+                        <i class="fas fa-drafting-compass"/>
                     </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times': 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active': 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/nosotros' className='nav-links' onClick={closeMobileMenu}>
+                                NOSOTROS
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/obras' className='nav-links' onClick={closeMobileMenu}>
+                                OBRAS
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/servicio' className='nav-links' onClick={closeMobileMenu}>
+                                SERVICIO
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/contacto' className='nav-links' onClick={closeMobileMenu}>
+                                CONTACTO
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/ingresa' className='nav-links-mobile' onClick={closeMobileMenu}>
+                                INGRESA
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline'>INGRESA</Button>}
                 </div>
             </nav>
         </>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
