44a2be5b0d30492a49554e21dea8ad9ea5a57814
Diego Sandoval <sandovdiego@gmail.com>
2021-01-14 01:00:23 -0300

    reorganizar dashboard

--- frontend/src/components/pages/system/Dashboard.js
+++ frontend/src/components/pages/system/Dashboard.js
@@ -180,22 +180,16 @@ export default function Dashboard() {
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={3}>
-            {/* Chart */}
-            <Grid item xs={12} md={8} lg={9}>
-              <Paper className={fixedHeightPaper}>
-                <Chart />
-              </Paper>
-            </Grid>
-            {/* Recent Deposits */}
-            <Grid item xs={12} md={4} lg={3}>
-              <Paper className={fixedHeightPaper}>
-                <Deposits />
-              </Paper>
-            </Grid>
             {/* Recent Orders */}
             <Grid item xs={12}>
               <Paper className={classes.paper}>
                 <Orders />
+              </Paper>
+            </Grid>
+                        {/* Chart */}
+                        <Grid item xs={12}>
+              <Paper className={fixedHeightPaper}>
+                <Chart />
               </Paper>
             </Grid>
           </Grid>
