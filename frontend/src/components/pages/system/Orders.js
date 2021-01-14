b3e74fd2e6dc9e0acd4ba7eb456160a49e364295
Diego Sandoval <sandovdiego@gmail.com>
2021-01-14 03:34:36 -0300

    delete dead code

--- frontend/src/components/pages/system/Orders.js
+++ frontend/src/components/pages/system/Orders.js
@@ -11,51 +11,6 @@ import axios from 'axios';
 
 import { render } from '@testing-library/react';
 
-/*
-const test = [
-  {
-    nombre: "Oficinas cocacola",
-    ubicacion: "Temuco",
-    fecha_i: "2020-10-03"
-  },
-  {
-    nombre: "Hotel Collin",
-    ubicacion: "Rio bueno",
-    fecha_i: "2020-10-03"
-  }
-];
-
-// Generate Order Data
-function createData(id, date, name, location,  amount) {
-  return { id, date, name, location, amount };
-}
-//const map1 = array1.map(x => x * 2);
-
-
-const rows = test.map((x, index) =>
-  createData(index, x.fecha_i , x.nombre, x.ubicacion, "10 dolares"));
-
-/*
-  createData(0, '16 Feb, 2019', test.nombre, test.ubicacion , "174.445.162"),
-  createData(1, '01 Mar, 2020', 'Biblioteca Municipal', 'Lautaro, Araucanía', "80.866.099"),
-  createData(2, '30 Mar, 2020', 'Cancha Sintética', 'Valdivia, Los Ríos', "15.200.581"),
-  createData(3, '18 Jun, 2020', 'Restaurant "Las Brazas"', 'Osorno, Los Lagos', "6.354.349"),
-  createData(4, '22 Dic, 2020', 'Puntos ecológicos', 'Panguipulli, Los Ríos', "1.212.379"),
-*/
-/*
-
-function preventDefault(event) {
-  event.preventDefault();
-}
-
-const useStyles = makeStyles((theme) => ({
-  seeMore: {
-    marginTop: theme.spacing(3),
-  },
-}));
-
-*/
-
 const useStyles = theme => ({
   seeMore: {
     marginTop: theme.spacing(3),
@@ -114,27 +69,4 @@ class Orders extends React.Component {
   }
 }
 
-
 export default withStyles(useStyles)(Orders)
-
-/*
-
-export default function Orders() {
-  const classes = useStyles();
-
-  useEffect( async () => {
-    try{
-        const response = await Api.get("/obra");
-        console.log("OBJETO FETCHEADO:");
-        console.log("OBJETO FETCHEADO:");
-        console.log(response);
-
-    }catch(err) {
-        console.log("Error retrieving data from API");
-        console.log(err);
-    }
-}, []);
-  
-}
-
-*/
